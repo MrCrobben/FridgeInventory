6924cc87cfb17fd645784fba8f5915c69c5ed827
MrCrobben <benjamin.beric@gmail.com>
2021-01-05 02:14:51 +0100

    Initial commit

---
+++ app/src/main/java/com/example/fridgeinventory/MainActivity.java
@@ -0,0 +1,62 @@
+package com.example.fridgeinventory;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.zxing.integration.android.IntentResult;
+import com.journeyapps.barcodescanner.*;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnScan,btnContent;
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnScan = findViewById(R.id.btn_scan);
+        btnContent = findViewById(R.id.btn_content);
+        textView = findViewById(R.id.txt_scan);
+
+        btnContent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,ScanActivity.class));
+            }
+        });
+        btnScan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new IntentIntegrator(MainActivity.this).setBeepEnabled(true).initiateScan(); // `this` is the current Activity
+
+
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if(result != null) {
+            if(result.getContents() == null) {
+                Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(this, "Scanned: " + result.getContents(), Toast.LENGTH_LONG).show();
+                textView.setText(result.getContents());
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+}
